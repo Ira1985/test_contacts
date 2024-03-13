ec33d1477a3a55a289862530577207a47846d803
Ira <i.zinko@xway.ru>
2024-03-13 14:36:35 +0300

    add fetch

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const responce = await fetch("https://randomuser.me/api/?results=200");
+      const { results } = await responce.json();
+      setContacts(results);
+    } catch (err) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div>...loading</div>;
+  }
+  if (isError) {
+    return <div>...error</div>;
+  }
+  return <div>Contacts! {contacts[0].name.first}</div>;
 };
