d457c95b8a415cf7e3bc54da77cb8175b8b56643
Ira <i.zinko@xway.ru>
2024-03-13 16:54:51 +0300

    refactor fetch

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,26 +1,34 @@
 import { useState, useEffect } from "react";
 
-export const Contacts = () => {
+const useContacts = () => {
   const [contacts, setContacts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      setIsLoading(true);
-      const responce = await fetch("https://randomuser.me/api/?results=200");
-      const { results } = await responce.json();
-      setContacts(results);
-    } catch (err) {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
-    fetchData();
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setContacts(results);
+      } catch (err) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
   }, []);
+
+  return { contacts, isLoading, isError };
+};
+
+export const Contacts = () => {
+  const { contacts, isLoading, isError } = useContacts();
   if (isLoading) {
     return <div>...loading</div>;
   }
